f9ddd56efad40298f11b5daf982baf5e7c92a923
serialcoder13 <kiran.grandhi@fincity.com>
2022-08-23 13:24:31 +0530

    Updated the setdata function to new simplified version.

--- __tests__/SetDataTest.ts
+++ __tests__/SetDataTest.ts
@@ -1,13 +1,14 @@
 import { setStoreData } from "../src/SetData";
 
-test('setData - Test', () => {
+test("setData - Test", () => {
+  let store: any = {};
 
-    let store:any = {};
+  setStoreData("Store.a", store, 20, "Store");
+  expect(store.a).toBe(20);
 
-    setStoreData("Bamboo.a", store, 20, "Bamboo");
-    expect(store.a).toBe(20);
+  setStoreData("Store.x.y[0]", store, 20, "Store");
+  expect(store.x.y[0]).toBe(20);
 
-    setStoreData("Bamboo.x.y[0]", store, 20, "Bamboo");
-    console.log(store);
-    expect(store.x.y[0]).toBe(20);
+  setStoreData("Store.z[Store.x.y[0]]", store, 300, "Store");
+  expect(store.z[20]).toBe(300);
 });
