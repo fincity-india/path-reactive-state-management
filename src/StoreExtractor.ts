c85bd176dc2977bebb3637db1ac0d42901acc61b
serialcoder13 <kiran.grandhi@fincity.com>
2024-02-21 08:20:42 +0530

    Upgraded KIRun version

--- src/StoreExtractor.ts
+++ src/StoreExtractor.ts
@@ -1,19 +1,21 @@
 import { TokenValueExtractor } from "@fincity/kirun-js";
 
 export class StoreExtractor extends TokenValueExtractor {
-    private store: any;
-    private prefix: string;
-    constructor(store: any, prefix: string) {
-        super();
-        this.store = store;
-        this.prefix = prefix;
-    }
-    protected getValueInternal(token: string) {
-        let parts: string[] = token.split(TokenValueExtractor.REGEX_DOT);
-        return this.retrieveElementFrom(token, parts, 1, this.store)
-    }
-    getPrefix(): string {
-        return this.prefix;
-    }
-    
+  private store: any;
+  private prefix: string;
+  constructor(store: any, prefix: string) {
+    super();
+    this.store = store;
+    this.prefix = prefix;
+  }
+  protected getValueInternal(token: string) {
+    let parts: string[] = token.split(TokenValueExtractor.REGEX_DOT);
+    return this.retrieveElementFrom(token, parts, 1, this.store);
+  }
+  getPrefix(): string {
+    return this.prefix;
+  }
+  getStore() {
+    return this.store;
+  }
 }
